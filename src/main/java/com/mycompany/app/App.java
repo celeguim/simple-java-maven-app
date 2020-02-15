07a239718c9a2609cc885ca94c960f5c255ecda1
Luiz Celeguim <celeguim@Luizs-MacBook-Pro.local>
2020-02-15 15:41:08 +0100

    Maven add repository/distributionManagement

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,21 +1,35 @@
 package com.mycompany.app;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 /**
  * Hello world!
  */
-public class App
-{
+public class App {
 
-    private final String message = "Hello World!";
+	private final String message = "Hello World!";
 
-    public App() {}
+	public App() {
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--headless");
+		System.setProperty("webdriver.chrome.driver", "/Users/celeguim/Downloads/chromedriver");
+		WebDriver webdriver = new ChromeDriver(chromeOptions);
+		webdriver.get("http://localhost:8080/");
+		webdriver.manage().logs();
+		webdriver.getPageSource();
+		webdriver.getTitle();
+		webdriver.findElement(By.id("j_username"));
+	}
 
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
+	public static void main(String[] args) {
+		System.out.println(new App().getMessage());
+	}
 
-    private final String getMessage() {
-        return message;
-    }
+	private final String getMessage() {
+		return message;
+	}
 
 }
